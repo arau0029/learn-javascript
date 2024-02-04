c6a12c43458fc73399934cd9b507761d04bb3b71
Gabriela Angel A R Silva <arau0029@algonquinlive.com>
2024-02-04 01:08:02 -0300

    completes exercises

--- 2. Practice time - part 1/01. Variables practice/index.js
+++ 2. Practice time - part 1/01. Variables practice/index.js
@@ -1,7 +1,12 @@
 // Create two variables, firstName and lastName
+const firstName = 'Gabriela Angel';
+const lastName = 'Silva';
 
 // Concatenate the two variables into a third variable called fullName
 
+const fullName = firstName + '' + lastName ;
+
 // Log fullName to the console
 
+console.log(fullName)
 
--- 2. Practice time - part 1/02. Concatenate two strings in a function/index.js
+++ 2. Practice time - part 1/02. Concatenate two strings in a function/index.js
@@ -3,3 +3,9 @@ let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
 
+function call () {
+    let calling = greeting + ', ' + name + '!';
+    console.log(calling);
+}
+
+call();
