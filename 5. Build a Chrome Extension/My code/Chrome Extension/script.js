b8c01e98d813cd09b962084491b74ff0a512259f
Gabriela Angel A R Silva <arau0029@algonquinlive.com>
2024-02-05 16:53:38 -0300

    prepares extension for development

---
+++ 5. Build a Chrome Extension/My code/Chrome Extension/script.js
@@ -0,0 +1,31 @@
+const inputBtn = document.getElementById('input-btn')
+let myLeads = []
+const inputEl = document.getElementById('input-el') 
+const ulEl = document.getElementById('ul-el')
+
+inputBtn.addEventListener('click', () => {
+    myLeads.push(inputEl.value)
+    inputEl.value = ''
+    renderLeads()
+})
+
+function renderLeads() {
+    let listItems = ''
+
+    for (let i = 0; i < myLeads.length; i++) {
+        // ulEl.innerHTML += `<li>${myLeads[i]}</li>`
+        // OR
+        // create element
+        // const li = document.createElement('li')
+        // li.textContent = myLeads[i]
+        // ulEL.append(li)
+
+        listItems += `
+            <li>
+                <a href="https://${myLeads[i]}" target="_blank">${myLeads[i]}</a>
+            </li>`
+    }
+
+    // We're doing this outside to improve performance; DOM manipulation comes with a cost
+    ulEl.innerHTML = listItems
+}
